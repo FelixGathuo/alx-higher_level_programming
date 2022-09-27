48b270000aded1c01773b43a7c65bc57699f1e05
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-27 15:20:24 +0300

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- script that converts database to UTF-8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name BLOB;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
