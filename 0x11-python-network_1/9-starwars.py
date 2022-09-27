3a60650cbba26bae0eba546af1b9e0e562fb6800
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-27 22:16:38 +0300

    Update 9-starwars.py

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
 """
-script that  takes in a string and sends a search request to the star wars API
+takes in a string and sends a search request to the Star Wars API
 """
-
-
-if __name__ == "__main__":
-    vals = {"search": argv[1] if len(argv) > 1 else ""}
-    try:
-        req = requests.get('https://swapi.co/api/people/', params=vals).json()
-        count = req.get("count")
-        print("Number of results: {}".format(count))
-        if count > 0:
-            for results in req.get("results"):
-                print(results["name"])
-    except ValueError:
-        print("Not a valid JSON")
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    r = requests.get('https://swapi.co/api/people', params={'search': argv[1]})
+    people = r.json()
+    print("Number of results: {}".format(people.get('count')))
+    for person in people.get('results'):
+        print(person.get('name'))
