70f1db31059ba8f20ebef146e13aa67a82225b80
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-27 21:49:51 +0300

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-from urllib.error import URLError, HTTPError
-from sys import argv
 """
-script that takes in url and sends a request to url
-displays body of response
+takes in a URL, sends a request to the URL & displays the body of the response
 """
-
-
 if __name__ == "__main__":
-    rep = urllib.request.Request(argv[1])
+    import urllib.error as error
+    import urllib.request as request
+    from sys import argv
+    req = request.Request(argv[1])
     try:
-        with urllib.request.urlopen(rep) as reply:
-            print(reply.read().decode(encoding="utf-8"))
-    except URLError as err:
-        print("Error code: {}".format(err.code))
+        with request.urlopen(req) as r:
+            print(r.read().decode('utf-8'))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
