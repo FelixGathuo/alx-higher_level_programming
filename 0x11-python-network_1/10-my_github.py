1cf3d4c2a603c9860974b609273a1b5bbd276c2f
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-27 22:25:21 +0300

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-import requests
-from sys import argv
 """
-script takes githut creds and uses to display id
+takes your Github credentials (username and password) and uses the Github API
+to display your id
 """
-
-
-if __name__ == "__main__":
-    uid = requests.get('https://api.github.com/user',
-                       auth=(argv[1], argv[2])).json()
-    if "id" in uid:
-        print(uid['id'])
-    else:
-        print(None)
+if __name__ == '__main__':
+    import requests
+    from requests.auth import HTTPBasicAuth
+    from sys import argv
+    r = requests.get('https://api.github.com/users/{}'.format(argv[1]),
+                     auth=HTTPBasicAuth(argv[1], argv[2]))
+    print(r.json().get('id'))
