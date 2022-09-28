7fa4876e652202e24ff55053a184c42757ce0318
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-28 12:15:12 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- creates a user
 -- Grants all privileges to the user
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
