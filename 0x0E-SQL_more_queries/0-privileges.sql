ecf6c141e3b287f49125496076654072a6b72c82
FelixGathuo <40053042+FelixGathuo@users.noreply.github.com>
2022-09-28 12:02:20 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- SQL query for listing users privileges
 -- Two users
-SHOW GRANTS FOR 'user_0d_1'@'localhost', 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
